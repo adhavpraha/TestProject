e83e1635195583f6eea8c72df38a014f70653403
vinayagamoorthi <adhav@vinayagamoorthis-MacBook-Air.local>
2026-06-13 13:02:07 +0530

    initial commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by vinayagamoorthi on 13/06/26.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
