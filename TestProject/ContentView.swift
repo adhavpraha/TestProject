08b7cfd3c4249bcf4e92238a7dd32a0b15cb948a
vinayagamoorthi <>
2026-07-18 11:57:11 +0530

    commt 5

--- TestProject/ContentView.swift
+++ TestProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Commit from source tree-july2026 comdtmitf1")
+            Text("Commit from sossurce tree-july2026 comdtmitf1")
         }
         .padding()
     }
